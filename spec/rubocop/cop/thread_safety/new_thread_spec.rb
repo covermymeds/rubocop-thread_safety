d769adcfdde773d19615c23331312cebd0bd7aeb
Bill Ruddock <bill.ruddock@gmail.com>
2020-06-25 18:17:31 +0100

    Match ::Thread.new from top-level constant

--- spec/rubocop/cop/thread_safety/new_thread_spec.rb
+++ spec/rubocop/cop/thread_safety/new_thread_spec.rb
@@ -10,6 +10,13 @@ RSpec.describe RuboCop::Cop::ThreadSafety::NewThread do
     RUBY
   end
 
+  it 'registers an offense for starting a new thread with top-level constant' do
+    expect_offense(<<-RUBY.strip_indent)
+      ::Thread.new { do_work }
+      ^^^^^^^^^^^^ Avoid starting new threads.
+    RUBY
+  end
+
   it 'does not register an offense for calling new on other classes' do
     expect_no_offenses('Other.new { do_work }')
   end
